131efd576955e3aec99258d1dc6a31435f2790cb
selimpcf <58636568+selimpcf@users.noreply.github.com>
2020-02-29 14:48:13 -0600

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -4,6 +4,6 @@ public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("hello hello selim");
         
-        //from Github
+          System.out.println("good bye git"); //this line is from github
     }
 }
